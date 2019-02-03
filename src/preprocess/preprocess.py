58be4b5035298b255e4e2b6f63fbaf578089549b
Hao Yang <haoyang.tem@gmail.com>
2019-02-02 22:09:07 -0800

    add bigram shingles in preprocess

--- src/preprocess/preprocess.py
+++ src/preprocess/preprocess.py
@@ -100,15 +100,16 @@ def preprocess_file(bucket_name, file_name):
     shingled_data = stemmed_data.withColumn("text_body_shingled", shingle("text_body_stemmed"))
 
     # timestamp
+    if (config.LOG_DEBUG): print("[PROCESSING]: Formatting unix_timestamp ...")
     final_data = shingled_data.withColumn("display_timestamp",unix_timestamp("display_date", "yyyyMMdd'T'HHmmss.SSS'Z'"))
 
     # Extract data that we want
     #final_data = shingled_data
     final_data.registerTempTable("final_data")
 
-
     preprocessed_data = sql_context.sql(
-        "SELECT id, headline, body, text_body, text_body_stemmed, text_body_shingled, hot, display_timestamp, djn_urgency from final_data")
+        "SELECT id, headline, body, text_body, text_body_tokenized, text_body_stemmed, \
+        text_body_shingled, hot, display_timestamp, djn_urgency from final_data")
 
     # Write to AWS
     if (config.LOG_DEBUG): print("[UPLOAD]: Writing preprocessed data to AWS...")
