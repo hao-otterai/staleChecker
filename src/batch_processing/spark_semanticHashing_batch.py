73b3a3be1a9381b172c6675f2be546dba77a6a1b
Hao Yang <haoyang.tem@gmail.com>
2019-02-03 23:39:01 -0800

    show number of partitions

--- src/batch_processing/spark_semanticHashing_batch.py
+++ src/batch_processing/spark_semanticHashing_batch.py
@@ -78,11 +78,8 @@ def find_dup_cands(mh, lsh):
         find_lsh_sim("q1.lsh_hash", "q2.lsh_hash").alias("lsh_sim")
     ).sort("q1_timestamp", "q2_timestamp")
 
-    if config.LOG_DEBUG: print(lsh_sim_df.count())
-
     # Duplicate candidates have a high enough LSH similarity count
     lsh_cand_df = lsh_sim_df.filter(lsh_sim_df.lsh_sim >= config.LSH_SIMILARITY_BAND_COUNT)
-    if config.LOG_DEBUG: print(lsh_cand_df.count())
 
     # Compare MinHash jaccard similarity scores for duplicate candidates
     find_mh_js = udf(lambda x, y: mh.jaccard_sim_score(x, y))
@@ -90,8 +87,8 @@ def find_dup_cands(mh, lsh):
 
     # Duplicate candidates need to have a high enough MinHash Jaccard similarity score
     dup_cand_df = mh_cand_df.filter(mh_cand_df.mh_js >= config.DUP_QUESTION_MIN_HASH_THRESHOLD)
-    if config.LOG_DEBUG: print(dup_cand_df.count())
 
+    print("number of partitions: ", dup_cand_df.rdd.getNumPartitions())
     dup_cand_df.foreachPartition(lambda rdd: store_dup_cand_redis(rdd))
 
 
