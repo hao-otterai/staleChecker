afe346499b64a8e8799139b1c31c70ed1ac06590
Hao Yang <haoyang.tem@gmail.com>
2019-02-04 13:09:37 -0800

    printSchema

--- src/preprocess/preprocess.py
+++ src/preprocess/preprocess.py
@@ -57,8 +57,8 @@ def get_two_gram_shingles(tokens):
 def get_tri_gram_shingles(tokens):
     return [(tokens[i], tokens[i + 1], tokens[i + 2]) for i in range(len(tokens) - 2)]
 
-def generate_tag(input_string):
-    return input_string.split('||') if len(input_string)>0 else ['<unspecified>']
+# def generate_tag(input_string):
+#     return input_string.split('||') if len(input_string)>0 else ['<unspecified>']
 
 # Preprocess a data file and upload it
 def preprocess_file(bucket_name, file_name):
@@ -79,11 +79,11 @@ def preprocess_file(bucket_name, file_name):
     partially_cleaned_data = raw_data.withColumn("cleaned_body", clean_body("body"))
 
     # generate tags based on company, industry, and market
-    if (config.LOG_DEBUG): print("[PROCESSING]: Generating news tags based on industry, market and company...")
-    tag_generator = udf(lambda input_string: generate_tag(input_string),  ArrayType(StringType()))
-    partially_cleaned_data = partially_cleaned_data.withColumn( "tag_company", tag_generator("company"))
-    partially_cleaned_data = partially_cleaned_data.withColumn( "tag_industry", tag_generator("industry"))
-    partially_cleaned_data = partially_cleaned_data.withColumn( "tag_market", tag_generator("market"))
+    # if (config.LOG_DEBUG): print("[PROCESSING]: Generating news tags based on industry, market and company...")
+    # tag_generator = udf(lambda input_string: generate_tag(input_string),  ArrayType(StringType()))
+    # partially_cleaned_data = partially_cleaned_data.withColumn( "tag_company", tag_generator("company"))
+    # partially_cleaned_data = partially_cleaned_data.withColumn( "tag_industry", tag_generator("industry"))
+    # partially_cleaned_data = partially_cleaned_data.withColumn( "tag_market", tag_generator("market"))
 
 
     # Concat cleaned question body and question title to form question vector
@@ -123,8 +123,7 @@ def preprocess_file(bucket_name, file_name):
     final_data.registerTempTable("final_data")
 
     preprocessed_data = sql_context.sql( "SELECT id, headline, body, text_body, text_body_stemmed, \
-        tag_company, tag_industry, tag_market, source, \
-        hot, display_date, display_timestamp, djn_urgency from final_data")
+        company, industry, market, source, hot, display_date, display_timestamp, djn_urgency from final_data")
 
     # Write to AWS
     if (config.LOG_DEBUG): print("[UPLOAD]: Writing preprocessed data to AWS...")
