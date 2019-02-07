b299816749dfa30fab580c2cfdfa412f3028fa35
Hao Yang <haoyang.tem@gmail.com>
2019-02-06 21:13:56 -0800

    meeh, hard to get it right with reducebykey....

--- src/batch_processing/spark_tag_indexed_brutal_v2.py
+++ src/batch_processing/spark_tag_indexed_brutal_v2.py
@@ -103,7 +103,7 @@ def find_similar_cand_with_lsh(df):
         return a
 
     _candidates_with_common_bucket = df.select(col('id'), col('headline'), col('min_hash'), col('lsh_hash')).rdd.flatMap(
-        lambda x: ((hash, band), [(x[0], x[1], x[2])]) for band, hash in enumerate(x[3])).reduceByKey(
+        lambda x: (((hash, band), [(x[0], x[1], x[2])]) for band, hash in enumerate(x[3]))).reduceByKey(
         lambda a, b: _extend(a,b)).filter(lambda x: len(x[1]) > 1).distinct()
     #.map(lambda x: tuple(x[1][n:n+3] for n in range(0,len(x[1]),3)))
     print(_candidates_with_common_bucket.collect())
