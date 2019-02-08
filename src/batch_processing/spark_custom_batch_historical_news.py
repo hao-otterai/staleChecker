84b754a6bb2d2275e7807f574e553eb78d0c7f4e
Hao Yang <haoyang.tem@gmail.com>
2019-02-07 17:12:38 -0800

    find_similar_cands_lsh debugging

--- src/batch_processing/spark_custom_batch_historical_news.py
+++ src/batch_processing/spark_custom_batch_historical_news.py
@@ -180,14 +180,14 @@ def find_similar_cands_lsh(df):
     if config.LOG_DEBUG: df.printSchema()
     rdd_cands_with_common_bucket = df.select(col('id'), col('min_hash'), col('headline'), col('timestamp'), col('lsh_hash')).rdd.flatMap(
         lambda x: (((hash, band), [(x[0], x[1], x[2], x[3])]) for band, hash in enumerate(x[4]))).reduceByKey(
-        lambda a, b: util.custom_extend(a,b))
+        lambda a, b: util.custom_extend(a,b)).filter(lambda x: len(x[1])>1).map(lambda x: tuple(x[1])).distinct()
     if config.LOG_DEBUG: print("find_similar_cands_lsh ==> {}".format(rdd_cands_with_common_bucket.collect()))
 
-    rdd_cands_with_common_bucket = rdd_cands_with_common_bucket.map(lambda x: tuple(x[1])).filter(lambda x: len(x)>1).distinct()
-    if config.LOG_DEBUG: print("find_similar_cands_lsh ==> {}".format(rdd_cands_with_common_bucket.collect()))
+    # rdd_cands_with_common_bucket = rdd_cands_with_common_bucket.map(lambda x: x[1]).filter(lambda x: len(x)>1).distinct()
+    # if config.LOG_DEBUG: print("find_similar_cands_lsh ==> {}".format(rdd_cands_with_common_bucket.first()))
 
     rdd_dataset = rdd_cands_with_common_bucket.map(lambda candiate_set: get_jaccard_similarity(candidate_set))
-    if config.LOG_DEBUG: print("find_similar_cands_lsh ==> {}".format(rdd_dataset.collect()))
+    if config.LOG_DEBUG: print("find_similar_cands_lsh ==> {}".format(rdd_dataset.first()))
 
     def _merge_result(acc_list, value_list):
         # Remove redundant similar sets from each partitions
