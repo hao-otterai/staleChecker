f0442d994865a0aa94f3b74264d124a3dc2f0651
Hao Yang <haoyang.tem@gmail.com>
2019-02-07 18:23:31 -0800

    code cleanup

--- src/batch_processing/spark_custom_batch_historical_news.py
+++ src/batch_processing/spark_custom_batch_historical_news.py
@@ -90,94 +90,12 @@ def find_dup_cands_within_tags():
 
         # find top similar set within a time window
         similar_sets_dict = find_similar_cands_lsh(tq_df)
-        print(similar_sets_dict)
-
-        # find_lsh_sim = udf(lambda x, y: util.common_bands_count(x, y), IntegerType())
-        # lsh_sim_df = tq_df.alias("q1").join(tq_df.alias("q2"),
-        #     [col("q1.timestamp") < col("q2.timestamp"),
-        #     col("q2.timestamp") - col("q1.timestamp") < config.TIME_WINDOW]).select(
-        #     col("q1.id").alias("q1_id"),
-        #     col("q2.id").alias("q2_id"),
-        #     col("q1.min_hash").alias("q1_min_hash"),
-        #     col("q2.min_hash").alias("q2_min_hash"),
-        #     col("q1.headline").alias("q1_headline"),
-        #     col("q2.headline").alias("q2_headline"),
-        #     col("q1.timestamp").alias("q1_timestamp"),
-        #     col("q2.timestamp").alias("q2_timestamp"),
-        #     find_lsh_sim("q1.lsh_hash", "q2.lsh_hash").alias("lsh_sim")
-        # ).sort("q1_timestamp", "q2_timestamp")
-        # # Duplicate candidates have a high enough LSH similarity count
-        # lsh_cand_df = lsh_sim_df.filter(lsh_sim_df.lsh_sim >= config.LSH_SIMILARITY_BAND_COUNT)
-        #
-        # # Compare MinHash jaccard similarity scores for duplicate candidates
-        # find_mh_js = udf(lambda x, y: util.jaccard_sim_score(x, y))
-        # mh_cand_df = lsh_cand_df.withColumn("mh_js", find_mh_js(lsh_cand_df.q1_min_hash, lsh_cand_df.q2_min_hash))
-        #
-        # # Duplicate candidates need to have a high enough MinHash Jaccard similarity score
-        # # dup_cand_df = mh_cand_df.filter(mh_cand_df.mh_js >= config.DUP_QUESTION_MIN_HASH_THRESHOLD)
-        #
-        # print("number of partitions: ", dup_cand_df.rdd.getNumPartitions())
-        # dup_cand_df.foreachPartition(lambda rdd: store_dup_cand_redis(rdd))
-
-
-# def find_similar_cands_lsh(df):
-#     # find set of news ids which have at least one common lsh bucket
-#     candidates_with_common_bucket = df.select(col('lsh_hash'), col('id')).rdd.flatMap(
-#         lambda x: (((hash, band), [x[1]]) for band, hash in enumerate(x[0]))).reduceByKey(
-#         lambda a, b: util._extend(a,b)).map(lambda x: tuple(x[1])).filter(lambda x: len(x)>1).distinct()
-#     if config.LOG_DEBUG: print("find_similar_cands_lsh ==> {}".format(candidates_with_common_bucket.first()))
-#
-#     rdd_dataset = candidates_with_common_bucket.map(lambda candiate_sets: get_jaccard_similarity(df, candidate_sets))
-#     if config.LOG_DEBUG: print("find_similar_cands_lsh ==> {}".format(rdd_dataset.first()))
-#
-#     similar_sets_dict = rdd_dataset.flatMap(lambda x: x.items()).reduceByKey(lambda acc, val: lsh.merge_result(acc, val)).collectAsMap()
-#     if config.LOG_DEBUG: print("find_similar_cands_lsh ==> {}".format(similar_sets_dict.first()))
-#     return similar_sets_dict
-
-# def get_jaccard_similarity(df, candidate_sets):
-#     start_time = time.time()
-#     rdb = redis.StrictRedis(config.REDIS_SERVER, port=6379, db=0)
-#     # Input whole df to calculate similar sets base on candidate_sets
-#     # create base set and its similar sets in a dictionary.
-#     # return = {base_set:(similar_set:jaccard_similarity, )}
-#     _similar_dict = {}
-#     if config.LOG_DEBUG: print('get_jaccard_similarity=>candidate_sets=%s'%(str(candidate_sets)))
-#
-#     # Generate combination for each set in candidate sets.
-#     #candidate_df = df.filter('id' in candidate_sets).orderBy(df.timestamp.asc()).collect()
-#     candidate_df = [df.filter(df.id == cand) for cand in candidate_sets]
-#     if config.LOG_DEBUG: print('get_jaccard_similarity=>candidate_df=%s'%(str(candidate_df)))
-#
-#     for _b_set, _s_set in itertools.permutations(candidate_df,2):
-#         _similar_dict[_b_set['id']] = []
-#         if _b_set['timestamp'] < _s_set['timestamp'] or _b_set['timestamp'] > (_s_set['timestamp'] + config.TIME_WINDOW):
-#             continue
-#
-#         #calculate jaccard similarity and update redis cache
-#         jaccard_sim_token = 'jaccard_sim:{}:{}'.format(_b_set['id'], _s_set['id'])
-#         if rdb.hexists(jaccard_sim_token):
-#             _jaccard_similarity = rdb.hget(jaccard_sim_token)
-#         else:
-#             _jaccard_similarity = util.jaccard_sim_score(_b_set['min_hash'], _s_set['min_hash'])
-#             rdb.hset(jaccard_sim_token, _jaccard_similarity)
-#
-#         # Store the result and get top NUM_OF_MOST_SIMILAR_SET similar sets
-#         if _jaccard_similarity > config.DUP_QUESTION_MIN_HASH_THRESHOLD:
-#             _similar_dict[_b_set['id']].append([_jaccard_similarity, _s_set['id']])
-#
-#     # filter and select top similar set.
-#     _similar_dict = dict( [(k,sorted(v, key=lambda x: (x[0],-int(x[1][1:])), reverse=True)[:config.NUM_OF_MOST_SIMILAR_SET])
-#                         for k,v in _similar_dict.items() if len(v)>0])
-#
-#     if DEBUG: print('get_jaccard_similarity=> _similar_dict=%s'%(_similar_dict))
-#     end_time = time.time()
-#     print("get_jaccard_similarity run time (seconds): {0} seconds".format(end_time - start_time))
-#     return _similar_dict
+        if config.LOG_DEBUG: print(similar_sets_dict)
 
 
 def find_similar_cands_lsh(df):
     # find set of news ids which have at least one common lsh bucket
-    if config.LOG_DEBUG: df.printSchema()
+    #if config.LOG_DEBUG: df.printSchema()
     rdd_cands_with_common_bucket = df.select(col('id'), col('min_hash'), col('headline'), col('timestamp'), col('lsh_hash')).rdd.flatMap(
         lambda x: (((hash, band), [(x[0], x[1], x[2], x[3])]) for band, hash in enumerate(x[4]))).reduceByKey(
         lambda a, b: util.custom_extend(a,b)).filter(lambda x: len(x[1])>1).map(lambda x: tuple(x[1]))
@@ -192,11 +110,11 @@ def find_similar_cands_lsh(df):
             if _v not in acc_list:
                 acc_list.append(_v)
             else: pass
-            if config.LOG_DEBUG > 1: print('LSH.get_merge_result=> _final_dict=%s'%(_final_dict))
+            #if config.LOG_DEBUG > 1: print('LSH.get_merge_result=> _final_dict=%s'%(_final_dict))
         return acc_list
 
     similar_sets_dict = rdd_dataset.flatMap(lambda x: x.items()).reduceByKey(lambda acc, val: _merge_result(acc, val)).collectAsMap()
-    if config.LOG_DEBUG: print("find_similar_cands_lsh ==> {}".format(similar_sets_dict))
+    #if config.LOG_DEBUG: print("find_similar_cands_lsh ==> {}".format(similar_sets_dict))
     return similar_sets_dict
 
 
@@ -209,7 +127,7 @@ def get_jaccard_similarity(candidate_set):
     start_time = time.time()
     rdb = redis.StrictRedis(config.REDIS_SERVER, port=6379, db=0)
     _similar_dict = {}
-    if config.LOG_DEBUG: print('get_jaccard_similarity=>candidate_set=%s'%(str(candidate_set)))
+    #if config.LOG_DEBUG: print('get_jaccard_similarity=>candidate_set=%s'%(str(candidate_set)))
 
     for _b_set, _s_set in itertools.permutations(candidate_set,2):
         _similar_dict[(_b_set[0],_b_set[2],_b_set[3])] = []
@@ -230,15 +148,11 @@ def get_jaccard_similarity(candidate_set):
     # filter and select top similar set.
     _similar_dict = dict( [(k,sorted(v, key=lambda x: (x[0],-x[1][2]), reverse=True)[:config.NUM_OF_MOST_SIMILAR_SET])
                         for k,v in _similar_dict.items() if len(v)>0])
-    if config.LOG_DEBUG: print('get_jaccard_similarity=> _similar_dict=%s'%(_similar_dict))
+    #if config.LOG_DEBUG: print('get_jaccard_similarity=> _similar_dict=%s'%(_similar_dict))
 
     end_time = time.time()
     print("get_jaccard_similarity run time (seconds): {0} seconds".format(end_time - start_time))
     return _similar_dict
-
-
-
-
 
 
 def main():
