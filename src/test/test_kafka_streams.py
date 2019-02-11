b609212c77c8c4483d5510667f828d6b635bb4dd
Hao Yang <haoyang.tem@gmail.com>
2019-02-10 19:58:15 -0800

    added schema for createDataFrame

--- src/test/test_kafka_streams.py
+++ src/test/test_kafka_streams.py
@@ -10,10 +10,16 @@ from pyspark.conf import SparkConf
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
 from pyspark.sql import SparkSession
+from pyspark.sql.types import *
 
 os.environ["PYSPARK_SUBMIT_ARGS"] = "--packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.2.0 pyspark-shell"
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/config")
 import config
+
+
+_fields = ['body', 'display_date', 'djn_urgency', 'headline', 'industry', 'id', 'source', 'hot', 'company', 'market']
+#fields = fieldList.map(lambda fieldName: StructField(fieldName, StringType(), nullable = true))
+_schema = StructType([StructField(_fieldName, StringType(), nullable = True) for _fieldName in _fields])
 
 
 # Converts incoming news, Adds timestamp to incoming question
@@ -39,14 +45,37 @@ def process(rdd):
         df.printSchema()
         print(df.first())
 
-def rdd2df(rdd):
+def rdd2df(rdd, schema):
     print("=========== rdd2df: Converting RDD[json] to DataFrame =========")
     spark = getSparkSessionInstance(rdd.context.getConf())
-    return  spark.createDataFrame(rdd)
+    return  spark.createDataFrame(rdd, _schema)
 
 
 
 def main():
+
+    ### add schema here
+    # {u'body': u" Fro ",
+    # u'display_date': u'20010126T031500.000Z',
+    # u'djn_urgency': u'0',
+    # u'headline': u'WSJ(1/26): Corrections & Amplifications',
+    # u'industry': u'uns',
+    # u'id': u'200110-6238',
+    # u'source': u'DJDN',
+    # u'hot': u'N',
+    # u'company': u'uns',
+    # u'market': u'M/NND;M/TPX'}
+
+    # |-- body: string (nullable = true)
+    # |-- company: string (nullable = true)
+    # |-- display_date: string (nullable = true)
+    # |-- djn_urgency: string (nullable = true)
+    # |-- headline: string (nullable = true)
+    # |-- hot: string (nullable = true)
+    # |-- id: string (nullable = true)
+    # |-- industry: string (nullable = true)
+    # |-- market: string (nullable = true)
+    # |-- source: string (nullable = true)
 
     spark_conf = SparkConf().setAppName("Spark Streaming Test")
     global sc
