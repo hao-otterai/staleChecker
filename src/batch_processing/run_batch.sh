9655ff348bf8799330789a1575a16d39bc0cfc68
Hao Yang <haoyang.tem@gmail.com>
2019-02-13 16:12:15 -0800

    .

--- src/batch_processing/run_batch.sh
+++ src/batch_processing/run_batch.sh
@@ -2,7 +2,7 @@
 
 # #remote
 # spark-submit --executor-memory 6G --master spark://ec2-35-155-171-170.us-west-2.compute.amazonaws.com:7077 \
-# src/batch_processing/batch_customMinHashLSH.py
+# src/batch_processing/batchCustomMinHashLSH.py
 
 # local
-spark-submit --executor-memory 6G src/batch_processing/batch_customMinHashLSH.py
+spark-submit --executor-memory 6G src/batch_processing/batchCustomMinHashLSH.py
