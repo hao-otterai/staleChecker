99238c59a81e68f4029c4745fccede1d7727d9f1
Hao Yang <haoyang.tem@gmail.com>
2019-02-14 20:25:05 -0800

    .

---
+++ src/clean.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+rm -rf preprocess/__pycache__
+rm -rf stream_processing/_spark_streaming_checkpoint
+rm -rf lib/*.pickle
+rm -rf lib/__pycache__
+rm -rf batch_processing/spark_warehouse
+rm -rf config/__pycache__
+rm -rf stream_processing/__pycache__
+rm -rf batch_processing/__pycache__
