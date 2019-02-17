bb7fe1f74d42ca86b922423db09948405d7cb0d6
Hao Yang <haoyang.tem@gmail.com>
2019-02-16 20:14:59 -0800

    streaming cleanup

--- src/stream_processing/spark_streaming.py
+++ src/stream_processing/spark_streaming.py
@@ -19,105 +19,114 @@ from pyspark.ml.feature import StopWordsRemover, Tokenizer
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/config")
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/lib")
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/preprocess")
+#sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/preprocess")
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/batch_processing")
 import config
 import util
-import locality_sensitive_hash
-import min_hash
-import preprocess
+#import locality_sensitive_hash
+#import min_hash
+#import preprocess
 import batchCustomMinHashLSH as batch_process
 
 
-# Lazily instantiated global instance of SparkSession
-def getSparkSessionInstance(sparkConf):
-    if ("sparkSessionSingletonInstance" not in globals()):
-        globals()["sparkSessionSingletonInstance"] = SparkSession.builder.config(conf=sparkConf).getOrCreate()
-    return globals()["sparkSessionSingletonInstance"]
-
-
-def conver_rdd_to_df(rdd, input_schema):
-    start_time = time.time()
-    if config.LOG_DEBUG: print("==== Converting RDD[json] to DataFrame ====")
-    spark = getSparkSessionInstance(rdd.context.getConf())
-    end_time = time.time()
-    if config.LOG_DEBUG: print("conver_rdd_to_df run time (seconds): {0} seconds".format(end_time - start_time))
-    return  spark.createDataFrame(rdd, input_schema)
+# schema for converting input news stream RDD[json] to DataFrame
+#input_schema = StructType([StructField(field, StringType(), nullable = True) for field in config.INPUT_SCHEMA_FIELDS])
 
 
 def save2redis(iter, news):
     rdb = redis.StrictRedis(config.REDIS_SERVER, port=6379, db=0)
     token = "dup_cand:{}".format(news['id'])
     for entry in iter:
-        #processed_timestamp = datetime.now().strftime("%Y-%m-%d %I:%M:%S %p")
-        dup = (news['headline'], news['timestamp'], entry.id, entry.headline, entry.timestamp)
-                    #news['ingest_timestamp'], processed_timestamp)
+        processed_timestamp = datetime.now().strftime("%Y-%m-%d %I:%M:%S %p")
+        dup = (news['headline'], news['timestamp'], entry.id, entry.headline, entry.timestamp,
+                    news['ingest_timestamp'], processed_timestamp)
         if config.LOG_DEBUG: print("saving dup_cand to Redis: {}".format(dup))
         rdb.zadd(token, entry.jaccard_sim, dup)
 
 
 
+def _ingest_timestamp(data):
+    output = data
+    output['ingest_timestamp'] = datetime.now().strftime("%Y-%m-%d %I:%M:%S %p")
+    return output
+
+
+
 def process_news(news, mh, lsh):
-    print('========= process_news  ========')
-    if config.LOG_DEBUG:
-        print('========= headline: {} ======='.format(news['headline']))
+    if config.LOG_DEBUG: print('========= process_news: {} ======='.format(news['headline']))
 
     q_timestamp = int(news['timestamp'])
     q_mh = mh.calc_min_hash_signature(news['text_body_stemmed'])
     q_lsh = lsh.find_lsh_buckets(q_mh)
 
     """
-    get the dataframe for all news of given tag.
-    id and lsh_hash columns loaded from Redis.
+    get the dataframe for all news of given tag. id and lsh_hash columns loaded from Redis.
     """
-    # rdb = redis.StrictRedis(config.REDIS_SERVER, port=6379, db=0)
-    # tq = []
-    # for tag in news['tag_company']:
-    #     #tq += rdb.zrangebyscore("lsh:{0}".format(tag),q_timestamp-config.TIME_WINDOW, q_timestamp, withscores=False)
-    #     for id in rdb.smembers("lsh:{}".format(tag)):
-    #         temp_lsh       = rdb.hget("news:{}".format(id), 'lsh_hash')
-    #         temp_mh        = rdb.hget("news:{}".format(id), 'min_hash')
-    #         temp_timestamp = rdb.hget("news:{}".format(id), 'timestamp')
-    #         if temp_lsh is not None and temp_mh is not None:
-    #             temp = {}
-    #             temp['id'] = id
-    #             temp['lsh_hash'] = [int(i) for i in temp_lsh.split(',')]
-    #             temp['min_hash'] = [int(i) for i in temp_mh.split(',')]
-    #             temp['timestamp'] = int(temp_timestamp)
-    #             tq.append(temp)
-    #         else:
-    #             print("Failed to get lsh_hash for news:{}".format(id))
-    # if len(tq) < 1: return
-    # if config.LOG_DEBUG: print("{0} historical news in the tag(s): {1}".format(len(tq), news['tag_company']))
-    # df = sql_context.read.json(sc.parallelize(tq))
-    #
-    # udf_num_common_buckets = udf(lambda x: util.sim_count(x, q_lsh), IntegerType())
-    # udf_get_jaccard_similarity = udf(lambda x: util.jaccard_sim_score(x, q_mh), FloatType())
-    # filtered_df = df.filter((col('timestamp') > (q_timestamp-config.TIME_WINDOW)) & (col('timestamp') < q_timestamp))\
-    #         .withColumn('common_buckets', udf_num_common_buckets('lsh_hash'))\
-    #         .filter( col('common_buckets') > config.LSH_SIMILARITY_BAND_COUNT)\
-    #         .withColumn( 'jaccard_sim', udf_get_jaccard_similarity('min_hash'))\
-    #         .filter( col('jaccard_sim') > config.DUP_QUESTION_MIN_HASH_THRESHOLD)
-    # #if config.LOG_DEBUG: filtered_df.count().map(lambda x: "{} similar news found".format(x))
-    # filtered_df.foreachPartition(lambda iter: save2redis(iter, news))
-    #
-    #
-    # """ Store tag + news in Redis """
-    # if config.LOG_DEBUG: print('========== Save news data to Redis =============')
-    # for tag in news['tag_company']:
-    #     # rdb.zadd("lsh:{}".format(tag), q.timestamp, json.dumps(news_data))
-    #     rdb.sadd("lsh:{}".format(tag), news['id'])
-    #     rdb.sadd("lsh_keys", "lsh:{}".format(tag))
-    # news_data = {
-    #                 "headline": news['headline'],
-    #                 "min_hash": ",".join([str(i) for i in q_mh]),
-    #                 "lsh_hash": ",".join([str(i) for i in q_lsh]),
-    #                 "timestamp": q_timestamp,
-    #                 "tag_company": ",".join(news["tag_company"])
-    #             }
-    # rdb.hmset("news:{}".format(news['id']), news_data)
+    rdb = redis.StrictRedis(config.REDIS_SERVER, port=6379, db=0)
+    tq = []
+    for tag in news['tag_company']:
+        #tq += rdb.zrangebyscore("lsh:{0}".format(tag),q_timestamp-config.TIME_WINDOW, q_timestamp, withscores=False)
+        for id in rdb.smembers("lsh:{}".format(tag)):
+            temp_lsh       = rdb.hget("news:{}".format(id), 'lsh_hash')
+            temp_mh        = rdb.hget("news:{}".format(id), 'min_hash')
+            temp_timestamp = rdb.hget("news:{}".format(id), 'timestamp')
+            if temp_lsh is not None and temp_mh is not None:
+                temp = {}
+                temp['id'] = id
+                temp['lsh_hash'] = [int(i) for i in temp_lsh.split(',')]
+                temp['min_hash'] = [int(i) for i in temp_mh.split(',')]
+                temp['timestamp'] = int(temp_timestamp)
+                tq.append(temp)
+            else:
+                print("Failed to get lsh_hash for news:{}".format(id))
+    if len(tq) < 1: return
+    if config.LOG_DEBUG: print("{0} historical news in the tag(s): {1}".format(len(tq), news['tag_company']))
+    df = sql_context.read.json(sc.parallelize(tq))
+
+    udf_num_common_buckets = udf(lambda x: util.sim_count(x, q_lsh), IntegerType())
+    udf_get_jaccard_similarity = udf(lambda x: util.jaccard_sim_score(x, q_mh), FloatType())
+    filtered_df = df.filter((col('timestamp') > (q_timestamp-config.TIME_WINDOW)) & (col('timestamp') < q_timestamp))\
+            .withColumn('common_buckets', udf_num_common_buckets('lsh_hash'))\
+            .filter( col('common_buckets') > config.LSH_SIMILARITY_BAND_COUNT)\
+            .withColumn( 'jaccard_sim', udf_get_jaccard_similarity('min_hash'))\
+            .filter( col('jaccard_sim') > config.DUP_QUESTION_MIN_HASH_THRESHOLD)
+    #if config.LOG_DEBUG: filtered_df.count().map(lambda x: "{} similar news found".format(x))
+    filtered_df.foreachPartition(lambda iter: save2redis(iter, news))
 
 
+    """ Store tag + news in Redis """
+    if config.LOG_DEBUG: print('========== Save news data to Redis =============')
+    for tag in news['tag_company']:
+        # rdb.zadd("lsh:{}".format(tag), q.timestamp, json.dumps(news_data))
+        rdb.sadd("lsh:{}".format(tag), news['id'])
+        rdb.sadd("lsh_keys", "lsh:{}".format(tag))
+    news_data = {
+                    "headline": news['headline'],
+                    "min_hash": ",".join([str(i) for i in q_mh]),
+                    "lsh_hash": ",".join([str(i) for i in q_lsh]),
+                    "timestamp": q_timestamp,
+                    "tag_company": ",".join(news["tag_company"])
+                }
+    rdb.hmset("news:{}".format(news['id']), news_data)
+
+
+
+# # Lazily instantiated global instance of SparkSession
+# def getSparkSessionInstance(sparkConf):
+#     if ("sparkSessionSingletonInstance" not in globals()):
+#         globals()["sparkSessionSingletonInstance"] = SparkSession.builder.config(conf=sparkConf).getOrCreate()
+#     return globals()["sparkSessionSingletonInstance"]
+#
+#
+
+
+# def conver_rdd_to_df(rdd, input_schema):
+#     start_time = time.time()
+#     if config.LOG_DEBUG: print("==== Converting RDD[json] to DataFrame ====")
+#     spark = getSparkSessionInstance(rdd.context.getConf())
+#     end_time = time.time()
+#     if config.LOG_DEBUG: print("conver_rdd_to_df run time (seconds): {0} seconds".format(end_time - start_time))
+#     return  spark.createDataFrame(rdd, input_schema)
 
 
 # def process_news(iter):
@@ -172,11 +181,11 @@ def main():
     spark_conf = SparkConf().setAppName("Spark Streaming MinHashLSH")
     global sc
     sc = SparkContext(conf=spark_conf)
-    sc.addFile(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/lib/locality_sensitive_hash.py")
-    sc.addFile(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/lib/min_hash.py")
+    #sc.addFile(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/lib/locality_sensitive_hash.py")
+    #sc.addFile(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/lib/min_hash.py")
     sc.addFile(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/lib/util.py")
     sc.addFile(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/config/config.py")
-    sc.addFile(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/preprocess/preprocess.py")
+    #sc.addFile(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/preprocess/preprocess.py")
     sc.addFile(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/batch_processing/batchCustomMinHashLSH.py")
 
     global sql_context
@@ -186,49 +195,17 @@ def main():
     ssc = StreamingContext(sc, config.SPARK_STREAMING_MINI_BATCH_WINDOW)
     #ssc.checkpoint("_spark_streaming_checkpoint")
 
+    mh, lsh = batch_process.load_mh_lsh()
+
     kafka_stream = KafkaUtils.createDirectStream( ssc,
                     [config.KAFKA_TOPIC],
                     {"metadata.broker.list": ",".join(config.KAFKA_SERVERS)} )
 
     kafka_stream.count().map(lambda x:('==== {} news in mini-batch ===='.format(x))).pprint()
 
-    mh, lsh = batch_process.load_mh_lsh()
-
-    # schema for converting input news stream RDD[json] to DataFrame
-    #input_schema = StructType([StructField(field, StringType(), nullable = True) for field in config.INPUT_SCHEMA_FIELDS])
-
-    def _ingest_timestamp(data):
-        output = data
-        #if config.LOG_DEBUG: print(type(data), data)
-        output['ingest_timestamp'] = datetime.now().strftime("%Y-%m-%d %I:%M:%S %p")
-        return output
-
-    def _process_mini_batch(rdd):
-        # rdd.foreachPartition(process_mini_batch)
-        rddc =  rdd.collect()
-        if config.LOG_DEBUG:
-            print("====== process_mini_batch: {} news======".format(len(rddc)))
-        for news in rddc:
-            if len(news) > 0:
-                process_news(news)
-
-    def _ingest_timestamp2(data):
-        return (data, datetime.now().strftime("%Y-%m-%d %I:%M:%S %p"))
-
-    def _helper(data):
-        process_news(data, mh, lsh)
-
-    # kafka_stream.map(lambda kafka_response: json.loads(kafka_response[1])).map(
-    #         lambda data: _ingest_timestamp(data)).foreachRDD(
-    #         lambda rdd: _process_mini_batch(rdd))
-
-    kafka_stream.map(lambda kafka_response: json.loads(kafka_response[1])).map(
-            lambda data: _ingest_timestamp(data)).foreachRDD(
-            lambda rdd: rdd.foreach(lambda data: process_news(data, mh, lsh)))
-
-    # kafka_stream.map(lambda kafka_response: kafka_response[1])\
-    #             .map(lambda data: _ingest_timestamp2(data))\
-    #             .foreachRDD(lambda rdd: rdd.foreach(_helper))
+    kafka_stream.map(lambda kafka_response: json.loads(kafka_response[1]))\
+                .map(lambda data: _ingest_timestamp(data))\
+                .foreachRDD( lambda rdd: rdd.foreach(lambda data: process_news(data, mh, lsh)))
 
     ssc.start()
     ssc.awaitTermination()
