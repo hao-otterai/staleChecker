9980cb45646c53e2746ec6f897b13f1ab85c8d54
Hao Yang <haoyang.tem@gmail.com>
2019-02-18 10:49:24 -0800

    .

--- flaskapp/app/test.py
+++ flaskapp/app/test.py
@@ -2,6 +2,7 @@ from app import app
 from flask import render_template, redirect
 from collections import Counter
 
+''' Basic Routes '''
 @app.route('/test')
 def index():
     return "Hello from flask!"
@@ -29,3 +30,50 @@ def about():
 @app.route("/metrics")
 def metrics():
     return render_template("metrics.html")
+
+
+''' util functions '''
+def convertUnixtimestamp(timestamp):
+    try:
+        timestamp = int(timestamp)
+        return datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+    except Exception as e:
+        return str(timestamp)
+
+''' Routes '''
+@app.route('/')
+@app.route('/latest')
+def latestNews():
+    rdb = redis.StrictRedis(REDIS_SERVER, port=6379, db=0)
+    ids = rdb.zrevrangebyscore("newsId", "+inf", 980380000, withscores=False)
+    output = []
+    for id in ids[:500]:
+        temp = {}
+        news = rdb.hgetall("news:{}".format(id))
+
+        if news is None: continue
+
+        temp['id'] = id
+        try:
+            temp['headline'] = news['headline']
+        except Exception as e:
+            continue
+        try:
+            temp['body'] = news['body']
+        except Exception as e:
+            pass
+        try:
+            temp['tag_company'] = news['tag_company'].split(',')
+        except Exception as e:
+            pass
+        try:
+            temp['timestamp'] = convertUnixtimestamp(news['timestamp'])
+        except Exception as e:
+            pass
+        temp['numDups'] = rdb.hlen("dup_cand:{}".format(id))
+        if temp['numDups'] > 0:
+            temp['dupCands'] = rdb.hgetall("dup_cand:{}".format(id))
+        else:
+            temp['dupCands'] = []
+        output.append(temp)
+    return render_template("news_list.html", dup_cands=output)
