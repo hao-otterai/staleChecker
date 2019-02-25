bf9550d5d6db02caacf04c78535347fe450d4e83
Hao Yang <haoyang.tem@gmail.com>
2019-02-25 14:34:34 -0800

    Update run_spark_streaming.sh

--- src/stream_processing/run_spark_streaming.sh
+++ src/stream_processing/run_spark_streaming.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# cluster
-#spark-submit --master spark://ip-10-0-0-10:7077 \
-#--packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.1.0 \
-#--jars /usr/lib/spark/lib/datanucleus-api-jdo-3.2.6.jar,/usr/lib/spark/lib/datanucleus-core-3.2.10.jar,/usr/lib/spark/lib/datanucleus-rdbms-3.2.9.jar \
-#spark_streaming.py
-
-# local
 spark-submit --master spark://ip-10-0-0-10.us-west-2.compute.internal:7077  --executor-memory 6G \
 --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.1.0  \
 src/stream_processing/spark_streaming.py
